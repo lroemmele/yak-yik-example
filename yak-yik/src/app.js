c48c8b612a85b64316736a1ad3e08faeafed573b
lroemmele <lroemmele@gmail.com>
2018-02-19 17:10:58 -0500

    add react user interface

---
+++ yak-yik/src/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+     render(){
+
+          return(
+               <div>
+                    Hello World
+               </div>
+          )
+     }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
